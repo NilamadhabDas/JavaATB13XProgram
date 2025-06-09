4f10f01c5e22cbaa3a5a5ce7bb2d59b4b3861bc6
NilamadhabDas <nilamadhab.005@gmail.com>
2025-06-09 22:20:12 +0530

    lab002

---
+++ src/Lab002_ShorcutHW.java
@@ -0,0 +1,12 @@
+public class Lab002_ShorcutHW {
+    public static void main(String[] args) {
+        System.out.println("Hello Test");
+    }
+
+    //    public static void main(String[] args) {
+//
+//    }
+    public static void main(int args) {
+
+    }
+}
