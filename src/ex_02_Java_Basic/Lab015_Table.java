39eee9e8cd0e26c4f75fe5754619ec7e582ce3f9
NilamadhabDas <nilamadhab.005@gmail.com>
2025-06-10 23:24:42 +0530

    Table

---
+++ src/ex_02_Java_Basic/Lab015_Table.java
@@ -0,0 +1,31 @@
+package ex_02_Java_Basic;
+
+public class Lab015_Table {
+    public static void main(String[] args) {
+        System.out.println("Printing table of 9 without using loops (just using printf())");
+        int a = 9;
+        int b = 1;
+        System.out.printf("%d x %d = %d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d x %d = %d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d x %d = %d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d x %d = %d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d x %d = %d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d x %d = %d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d x %d = %d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d x %d = %d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d x %d = %d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d x %d = %d\n", a, b, a * b);
+        b++;
+
+    }
+
+}
